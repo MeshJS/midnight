cd371bcc024a750ec7eba58e8a45d7d11c6ef0f6
erick <erickrs2esuarez@gmail.com>
2025-04-06 11:01:16 -0300

    wallet widget hooks doc

--- example-workspaces/wallet-examples/src/pages/index.tsx
+++ example-workspaces/wallet-examples/src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { ModeToggle } from "@/components/mode-toggle";
-import { MidnightWallet, useAssets } from "@meshsdk/midnight-react";
+import { MidnightWallet, useAssets, useWallet } from "@meshsdk/midnight-react";
+// import { useWalletList, useWalletBalanceProve, useWalletSubmit } from "@meshsdk/midnight-react";
 import { MidnightWallet as HeadlessWallet } from "@/components/wallet-widget/midnight-wallet";
 
 export default function Home() {
-  const {address} = useAssets();
+  const {address, coinPublicKey, encryptionPublicKey, hasConnectedWallet, isProofServerOnline, uris, walletName} = useAssets();
+  const { connectingWallet, disconnect, setOpen, connectWallet } = useWallet();
+  // const walletList = useWalletList();
+  // const {result, submitTx, submitting} = useWalletBalanceProve();
+  // const { result, submitTx, submitting } = useWalletSubmit();
+
   return (
     <>
       <ModeToggle />
@@ -11,7 +17,26 @@ export default function Home() {
       <MidnightWallet />
       <div>This is a Headless Wallet</div>
       <HeadlessWallet />
-      {address}
+      <div>These are the kooks provided by useAssets</div>
+      <div>
+        <div>Wallet Name: {walletName}</div>
+        <div>Address: {address}</div>
+        <div>Coin Public Key: {coinPublicKey}</div>
+        <div>Encryption Public Key: {encryptionPublicKey}</div>
+        <div>Has Connected Wallet: {hasConnectedWallet ? "Yes" : "No"}</div>
+        <div>Is Proof Server Online: {isProofServerOnline ? "Yes" : "No"}</div>
+        <div>Indexer: {uris?.indexerUri}</div>
+        <div>IndexerWS: {uris?.indexerWsUri}</div>
+        <div>Proof Server: {uris?.proverServerUri}</div>
+        <div>Node: {uris?.substrateNodeUri}</div>
+      </div>      
+      <div>These are the kooks provided by useWallet</div>
+      <div>
+        <div>Connecting Wallet: {connectingWallet? "Yes" : "No"}</div>
+        <div onClick={disconnect}>Disconnect Wallet</div>
+        <div onClick={() => setOpen(true)}>Open Wallet Dialog</div>
+        <div onClick={() => connectWallet("mnLace")}>Connect to the Lace Wallet</div>
+      </div>      
     </>
   );
-}
+} 
