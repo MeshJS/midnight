a8ee2cf258a45c811b0f4ea05cfd4d512cca44e4
erick <erickrs2esuarez@gmail.com>
2025-07-31 06:19:56 -0300

    correct lint config

--- apps/playground/frontend/src/global.ts
+++ apps/playground/frontend/src/global.ts
@@ -1,6 +1,6 @@
 import { Buffer } from 'buffer';
 
-// @ts-expect-error 
+// @ts-expect-error - We're adding process.env to globalThis for compatibility with Node.js modules
 globalThis.process = {
   env: {
     NODE_ENV: import.meta.env.MODE, // Map `MODE` to `process.env.NODE_ENV`.
