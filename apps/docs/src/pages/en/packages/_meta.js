06b28a7c813524a37b3b299da8aa231ef0d403fe
luislucena16 <lucenaluis137@gmail.com>
2025-10-24 21:56:14 +0200

    chore: add complete midnight-contracts-wizard docs

--- apps/docs/src/pages/en/packages/_meta.js
+++ apps/docs/src/pages/en/packages/_meta.js
@@ -1,4 +1,12 @@
 export default {
+  "midnight_setup": {
+    title: "Midnight Setup",
+    route: "/midnight_setup",
+  },
+  "midnight_contracts_wizard": {
+    title: "Contracts Wizard",
+    route: "/midnight_contracts_wizard",
+  },
   "wallet_widget": {
     title: "Wallet Widget",
     route: "/wallet_widget",
---
+++ apps/docs/src/pages/en/packages/midnight_contracts_wizard/_meta.js
@@ -0,0 +1,23 @@
+export default {
+  "index": {
+    title: "Introduction",
+    route: "/",
+  },
+  "installation": {
+    title: "Installation",
+    route: "/installation",
+  },
+  "usage": {
+    title: "Usage",
+    route: "/usage",
+  },
+  "contracts": {
+    title: "Available Contracts",
+    route: "/contracts",
+  },
+  "project_structure": {
+    title: "Project Structure",
+    route: "/project_structure",
+  },
+};
+
